1af859e0f05456031bf10f098c2e78b81e6824f9
IF-tech <ianfloydtechnology@gmail.com>
2023-07-09 19:08:22 -0700

    Wrote Test Boiler

---
+++ test/dapp_chat.js
@@ -0,0 +1,27 @@
+// Import assert from Chai
+const assert = require("chai").assert;
+
+// Import the contract artifact
+const DappChat = artifacts.require("./DappChat.sol");
+
+contract("DappChat", (accounts) => {
+   let instance;
+
+   before(async () => {
+      instance = await DappChat.deployed();
+   });
+
+   describe("deployment", async () => {
+      it("deploys successfully", async () => {
+         const address = await instance.address;
+         assert.notEqual(address, 0x0);
+         assert.notEqual(address, "");
+         assert.notEqual(address, null);
+         assert.notEqual(address, undefined);
+      });
+
+      // More tests...
+   });
+
+   // More `describe` blocks...
+});
